82b1c22401d6ca4f007fac878e1ba968e3077fd4
Chenghan Sun <sunchenghan1995@gmail.com>
2020-05-07 14:27:44 -0700

    update SGD algorithm

--- Project-2/CS_NC_code_hw2.R
+++ Project-2/CS_NC_code_hw2.R
@@ -93,6 +93,7 @@ print(paste("The step size candidate = ", selected_eta))
 
 # Here implement the gradient descent algorithm 
 gradient_descent = function(data_X, res_y, eta=5.31e-05, grad_tol=10^(-5), maxiters=10^4, standardize=TRUE) {
+  # Implement based on page.3 Algorithm 1 of OPT.pdf
   # Params:
     # data_X: input data as a matrix
     # res_y: observation variables 
@@ -133,14 +134,9 @@ gradient_descent = function(data_X, res_y, eta=5.31e-05, grad_tol=10^(-5), maxit
       break
     }
     iter = iter + 1
-    # print(iter)
   }
   return(c(para_theta, iter))
 }
-
-# Apply gradient descent algorithm for training set based on model from part (a)
-res_y_train = src_train$price
-grad_results = gradient_descent(data_X_sd_global, res_y_train)
 
 # Here defined a function for computing the R^2 
 calc_r_square = function(fit_y, res_y) {
@@ -150,6 +146,10 @@ calc_r_square = function(fit_y, res_y) {
   model_r_2 = 1 - (modelSSR / modelSSTO)  # compute r^2
   return(model_r_2)
 }
+
+# Apply gradient descent algorithm for training set based on model from part (a)
+res_y_train = src_train$price
+grad_results = gradient_descent(data_X_global, res_y_train)  # input data X w/o standardization
 
 # Calculate R^2 of the original model for training set
 gd_train_a = as.numeric(data_X_sd_global %*% as.matrix(grad_results[1:5]))  # X*theta
@@ -161,10 +161,67 @@ print(paste("The R^2 of the original model for training set using gradient desce
 
 
 ############ Part (e) ############
+# Perform all the things above now using stochastic gradient descent (with one sample in each iteration).
 
+# Here implement the stochastic gradient descent algorithm 
+stochastic_gd = function(data_X, res_y, const_C, sgd_tol=10^(-4), maxiters=10^4, standardize=TRUE) {
+  # a version of mini-batch stochastic gradient descent
+  # Params:
+    # data_X: input data as a matrix
+    # res_y: observation variables 
+    # eta: step size --> a constant 
+    # grad_tol: tolerance of the error difference for norm of gradient 
+    # maxiters: maximum number of iterations 
+    # standardize: standardize the dataset for better gradient descent algorithm performance 
+  # Return:
+    # a list of: 1. estimated theta vector; 2: number of iterations 
+  if (standardize == TRUE) { # check standardization as GD algorithm
+    data_X_sd = scale(data_X)
+    data_X_sd = cbind(1, data_X_sd[, -1])  # make a design matrix by adding a column of 1's
+  }
+  else {
+    print(paste("Warning: Input data matrix X was not standardized."))
+  }
+  
+  n = nrow(data_X_sd)  # number of rows 
+  p = ncol(data_X_sd)  # number of columns
+  para_theta = as.matrix(rep(0, p))  # initialize theta vector with 0's
+  
+  iter = 0  # initialize iter number 
+  sample_pool = sample(n, replace=FALSE)  # make a list of sample integer from n w/o replacement 
+  
+  # generate decreasing step size eta_t based on conclusion of page.17 of OPT.pdf
+  for (i in 1:maxiters) {
+    iter = iter + 1  # update iter number at beginning
+    eta_t = const_C / (iter + 1)  # decreasing step size based on C and iter number
+    sample = sample_pool[iter]  # iterate each randomized sample from sample pool
+    print(paste("The selected sample = ", sample))
+    samp_data_X_sd = data_X_sd[sample, ]
+    samp_res_y = res_y[sample]
+    para_theta_old = para_theta  # store old theta into another variable 
+    
+    res_direct = as.numeric(samp_data_X_sd %*% para_theta_old) - samp_res_y  # compute response direction 
+    # print(res_direct)
+    gradient = as.numeric(res_direct %*% samp_data_X_sd)  # compute gradient
+    para_theta = para_theta_old - eta_t*gradient  # update estimator
+    # print(para_theta)
+    
+    if (iter > maxiters) {
+      print(paste("Algorithm unfinished by reaching the maximum iterations."))
+      break
+    }
+    # if ()
+  }
+  return(c(para_theta, iter))
+}
 
+# Apply gradient descent algorithm for training set based on model from part (a)
+sgd_results = stochastic_gd(data_X_global, res_y_train, 5)
 
-
+# Calculate R^2 of the original model for training set
+sgd_train_a = as.numeric(data_X_sd_global %*% as.matrix(sgd_results[1:5]))  # X*theta
+sgd_train_r_2_a = calc_r_square(sgd_train_a, res_y_train)
+print(paste("The R^2 of the original model for training set using stochastic gradient descent algorithm = ", sgd_train_r_2_a))
 
 
 
