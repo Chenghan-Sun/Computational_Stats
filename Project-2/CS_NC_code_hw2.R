80137d8a6bee2f729575e552ac9a062dafe5f30b
Chenghan Sun <sunchenghan1995@gmail.com>
2020-05-08 04:20:04 -0700

    fixed multiple issues

--- Project-2/CS_NC_code_hw2.R
+++ Project-2/CS_NC_code_hw2.R
@@ -207,7 +207,7 @@ gd_train_r_2_c = calc_r_square(gd_train_c, res_y_train_c)
 print(paste("The R^2 of the improved model C for training set using GD = ", gd_train_r_2_c))
 print(paste("The number of steps to finish the algorithm = ", grad_results_train_c_gd[7]))
 # output:
-# [1] "The R^2 of the linear model C for training set using GD =  0.51735329277383"
+# [1] "The R^2 of the improved model C for training set using GD =  0.51735329277383"
 # [1] "The number of steps to finish the algorithm =  1448"
 
 # Part-d-5: Calculate R^2 of the improved model C for testing set
@@ -219,7 +219,7 @@ gd_test_c = as.numeric(data_X_test_sd_c_global %*% as.matrix(grad_results_train_
 gd_test_r_2_c = calc_r_square(gd_test_c, res_y_test)
 print(paste("The R^2 of the improved model C for testing set using GD = ", gd_test_r_2_c))
 # output:
-# [1] "The R^2 of the linear model C for testing set using GD =  0.51052055091153"
+# [1] "The R^2 of the improved model C for testing set using GD =  0.51052055091153"
 
 # Part-d-6: estimate BC's house price based on GD
 fancy_c_global = cbind(as.matrix(src_fancy[, features]), src_fancy$bedrooms*src_fancy$bathrooms)  # extract target features 
@@ -237,7 +237,7 @@ print(paste("The estimated BC's house price from Improved model C for training s
 # [1] "The estimated BC's house price from Improved model C for training set using GD =  18607312.8904335"
 
 # Summary:
-table_model_A = data.frame("Step size A" = selected_eta,
+table_model_A_gd = data.frame("Step size A" = selected_eta,
                            "Actual Iteration Numbers A" = grad_results_train_a_gd[6], 
                            "Tolerance A" = 10^(-8),
                            "Maximum iterations A" = 10^4,
@@ -245,7 +245,7 @@ table_model_A = data.frame("Step size A" = selected_eta,
                            "Testing R^2 A" = gd_test_r_2_a,
                            "BC's house price A" = bchp_a)
 
-table_model_C = data.frame("Step size C" = selected_eta_c,
+table_model_C_gd = data.frame("Step size C" = selected_eta_c,
                            "Actual Iteration Numbers C" = grad_results_train_c_gd[7], 
                            "Tolerance C" = 10^(-8),
                            "Maximum iterations C" = 10^4,
@@ -258,37 +258,48 @@ table_model_C = data.frame("Step size C" = selected_eta_c,
 # Perform all the things above now using stochastic gradient descent (with one sample in each iteration).
 
 # Here implement the stochastic gradient descent algorithm 
-stochastic_gd = function(data_X, res_y, const_C, true_theta, sgd_tol=10^(-4), sgd_maxiters=10^5, standardize=TRUE) {
+stochastic_gd = function(data_X, data_X_test, res_y, res_y_test, const_C, sgd_tol=10^(-9), 
+                         sgd_maxiters=10^5, standardize=TRUE, plot_loss=TRUE) {
   # a version of mini-batch stochastic gradient descent
   # Params:
     # data_X: input data as a matrix
-    # res_y: observation variables 
+    # data_X_test: test dataset for plotting loss function
+    # res_y: observation variables
+    # res_y_test: test set observation variables for plotting loss function
     # const_C: a tuning constant, used to get step size 
-    # true_theta: theta from lm() build-in function
     # sgd_tol: tolerance of the error difference
     # maxiters: maximum number of iterations 
     # standardize: standardize the dataset for better gradient descent algorithm performance 
+    # plot_loss: bool controls if plot loss functionas steps 
   # Return:
     # a list of: 1. estimated theta vector; 2: number of iterations 
-  if (standardize == TRUE) { # check standardization as GD algorithm
-    data_X_sd = scale(data_X)
+  if (standardize == TRUE) { # check standardization of SGD algorithm
+    data_X_sd = scale(data_X)  # data_X_train
     data_X_sd = cbind(1, data_X_sd[, -1])  # make a design matrix by adding a column of 1's
+    data_X_sd_test = scale(data_X_test)  # for test dataset 
+    data_X_sd_test = cbind(1, data_X_sd_test[, -1])  # make a design matrix by adding a column of 1's
+    res_y_sd = scale(res_y)
+    res_y_test_sd = scale(res_y_test)
   }
   else {
     print(paste("Warning: Input data matrix X was not standardized."))
+    break
   }
   
-  n = nrow(data_X_sd)  # number of rows 
+  n = nrow(data_X_sd)  # number of rows
   p = ncol(data_X_sd)  # number of columns
   para_theta = as.matrix(rep(0, p))  # initialize theta vector with 0's
+  loss_train_vec = c()  # initialize loss function vectors
+  loss_test_vec = c()
   
   iter = 0  # initialize iter number 
+  iter_vec = c()
   sample_pool = sample(n, replace=FALSE)  # make a list of sample integer from n w/o replacement 
   
   # generate decreasing step size eta_t based on conclusion of page.17 of OPT.pdf
   for (i in 1:sgd_maxiters) {
     iter = iter + 1  # update iter number at beginning
-    print(paste("Iteration number: ", iter))
+    # print(paste("Iteration number: ", iter))
     eta_t = const_C / (iter + 1)  # decreasing step size based on C and iter number
     
     # select sample (based on given batch size)
@@ -299,49 +310,112 @@ stochastic_gd = function(data_X, res_y, const_C, true_theta, sgd_tol=10^(-4), sg
     else {
       sample = sample(sample_pool, 1)  # in case of run out of samples 
     }
-    
+    para_theta_old = para_theta
     samp_data_X_sd = data_X_sd[sample, ]
-    samp_res_y = res_y[sample]
-    res_direct = as.numeric(samp_data_X_sd %*% para_theta) - samp_res_y  # compute response direction 
+    samp_res_y = res_y_sd[sample]
+    res_direct = as.numeric(samp_data_X_sd %*% para_theta_old) - samp_res_y  # compute response direction 
     gradient = as.numeric(res_direct %*% samp_data_X_sd)  # compute gradient
-    para_theta = para_theta - eta_t*gradient  # update estimation of theta vector
-    # print(para_theta)
+    para_theta = para_theta_old - eta_t*gradient  # update estimation of theta vector
     
     if (iter > sgd_maxiters) {
       print(paste("Algorithm unfinished by reaching the maximum iterations."))
       break
     }
     
-    rel_err = norm((para_theta - true_theta), "2") / norm(as.matrix(true_theta), "2")
-    print(rel_err)
-    if (rel_err <= sgd_tol) {
+    err = sqrt(sum((as.vector(para_theta) - as.vector(para_theta_old))^2))
+    # print(err)
+    if (err <= sgd_tol) {
       print(paste("Algorithm finished by reaching the tolerance."))
       break
     }
-    # 
+    # loss function data update
+    loss_train = norm(res_y_sd - data_X_sd %*% para_theta, "2")
+    # print(loss_train)
+    loss_test = norm(res_y_test_sd - data_X_sd_test %*% para_theta, "2")
+    loss_train_vec = c(loss_train_vec, loss_train)  
+    loss_test_vec = c(loss_test_vec, loss_test)
+    iter_vec = c(iter_vec, iter)
   }
-  return(c(para_theta, iter))
+  return(list(para_theta, iter, data.frame(iter_vec, loss_train_vec,iter_vec, loss_test_vec)))
 }
 
-# get the True theta from lm() build-in function
-true_theta_a = summary(linear_model_a)$coefficients[,1]
-true_theta_c = summary(linear_model_c)$coefficients[,1]
+# Part-e-1: Calculate R^2 of the improved model A for training set
+# Apply sgd algorithm for training set based on model from part (a)
+sgd_results_train_a = stochastic_gd(cbind(1,scale(data_X_train_a_global[,-1])), data_X_test_sd_a_global, 
+                                    res_y_train_a, res_y_test, 
+                                    1)  # input data X w/o standardization
 
-# Part-d-1: Calculate R^2 of the linear model A for training set
-# Apply gradient descent algorithm for training set based on model from part (a)
-res_y_train_a = src_train$price
-sgd_results_train_a = stochastic_gd(data_X_train_a_global, res_y_train_a, 1, true_theta_a)  # input data X w/o standardization
 sgd_train_a = as.numeric(data_X_train_sd_a_global %*% as.matrix(sgd_results_train_a[1:5]))  # X*theta
-sgd_train_r_2_a = calc_r_square(sgd_train_a, res_y_train_a)
+sgd_train_r_2_a = calc_r_square(sgd_train_a, scale(res_y_train_a))
 print(paste("The R^2 of the linear model A for training set using SGD = ", sgd_train_r_2_a))
 print(paste("The number of steps to finish the algorithm = ", sgd_results_train_a[6]))
 # output:
-# [1] "The R^2 of the linear model A for training set using GD =  0.510113853079458"
-# [1] "The number of steps to finish the algorithm =  150"
+# [1] "The R^2 of the linear model A for training set using SGD =  0.509856890105469"
+# [1] "The number of steps to finish the algorithm =  20157"
 
+# plot loss function
+library(ggplot2)
+plot_data = as.data.frame(sgd_results_train_a[[3]])
+ggplot(plot_data)+geom_line(aes(plot_data$iter_vec, plot_data$loss_train_vec, color = "train")) + 
+  geom_line(aes(plot_data$iter_vec.1, plot_data$loss_test_vec, color = "test"))+
+  xlab("Iterations") + ylab("Loss")
 
+# Part-e-2: Calculate R^2 of the improved model A for testing set
+sgd_test_a = as.numeric(data_X_test_sd_a_global %*% as.matrix(sgd_results_train_a[1:5]))  # X*theta
+sgd_test_r_2_a = calc_r_square(sgd_test_a, res_y_test)
+print(paste("The R^2 of the improved model A for testing set using SGD = ", sgd_test_r_2_a))
+# output:
+# [1] "The R^2 of the improved model A for testing set using SGD =  0.502857392565492"
 
+# Part-e-3: estimate BC's house price based on GD
+bchp_a_sgd = as.numeric(fancy_sd_a_global %*% as.matrix(sgd_results_train_a[1:5])) # Fit for Gates house
+print(paste("The estimated BC's house price from linear model A for training set using SGD = ", bchp_a_sgd))
+# output
+# [1] "The estimated BC's house price from linear model A for training set using SGD =  15624675.3834043"
 
+# Repeat the strategy above:
+# Part-e-4: Calculate R^2 of the improved model C for training set
+# Apply sgd algorithm for training set based on model from part (c)
+sgd_results_train_c = stochastic_gd(data_X_train_c_global, data_X_test_sd_c_global, 
+                                    res_y_train_c, res_y_test, 
+                                    1)  # input data X w/o standardization
 
+sgd_train_c = as.numeric(data_X_train_sd_c_global %*% as.matrix(sgd_results_train_c[1:6]))  # X*theta
+sgd_train_r_2_c = calc_r_square(sgd_train_c, res_y_train_c)
+print(paste("The R^2 of the improved model C for training set using SGD = ", sgd_train_r_2_c))
+print(paste("The number of steps to finish the algorithm = ", sgd_results_train_c[7]))
 
+# output:
+# [1] "The R^2 of the improved model C for training set using SGD =  0.503247973101909"
+# [1] "The number of steps to finish the algorithm =  10000"
+
+# Part-e-5: Calculate R^2 of the improved model C for testing set
+sgd_test_c = as.numeric(data_X_test_sd_c_global %*% as.matrix(sgd_results_train_c[1:6]))  # X*theta
+sgd_test_r_2_c = calc_r_square(sgd_test_c, res_y_test)
+print(paste("The R^2 of the improved model C for testing set using SGD = ", sgd_test_r_2_c))
+# output:
+# [1] "The R^2 of the improved model C for testing set using SGD =  0.496604307026135"
+
+# Part-e-6: estimate BC's house price based on SGD
+bchp_c_sgd = as.numeric(fancy_sd_c_global %*% as.matrix(sgd_results_train_c[1:6])) # Fit for Gates house
+print(paste("The estimated BC's house price from Improved model C for training set using SGD = ", bchp_c_sgd))
+# output
+# [1] "The estimated BC's house price from Improved model C for training set using SGD =  16095069.4894778"
+
+# Summary:
+table_model_A_sgd = data.frame("Tuning Constant C" = 1,
+                           "Actual Iteration Numbers A" = sgd_results_train_a[6], 
+                           "Tolerance A" = 10^(-3),
+                           "Maximum iterations A" = 10^4,
+                           "Training R^2 A" = sgd_train_r_2_a,
+                           "Testing R^2 A" = sgd_train_r_2_c,
+                           "BC's house price A" = bchp_a_sgd)
+
+table_model_C_sgd = data.frame("Tuning Constant C" = 1,
+                           "Actual Iteration Numbers C" = sgd_results_train_c[7], 
+                           "Tolerance C" = 10^(-3),
+                           "Maximum iterations C" = 10^4,
+                           "Training R^2 C" = sgd_test_r_2_a,
+                           "Testing R^2 C" = sgd_test_r_2_c,
+                           "BC's house price C" = bchp_c_sgd)
 
